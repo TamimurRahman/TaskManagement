411564f88b5b24dcb409e782b8ac178cb1736f71
TamimurRahman <tamimr008@gmail.com>
2025-11-17 01:35:32 +0600

    user validation and authentication and authorization

--- todo/task/urls.py
+++ todo/task/urls.py
@@ -1,6 +1,13 @@
 
 from django.urls import path
-
+from .views import task_list,task_detail,task_delete,task_mark_compleated,register,task_create,user_login,logout_view
 urlpatterns = [
-    
+    path('',task_list,name='task_list'), 
+    path('create/',task_create,name='taskCreate'),
+    path('task/<int:task_id>',task_detail,name='task_detail'),
+    path('task/<int:task_id>/delete/',task_delete,name='task_delete'),
+    path('task/<int:task_id>/compleate/',task_mark_compleated,name='mark_compleate'),
+    path('register/',register,name='register'),
+    path('login/',user_login,name='login'),
+    path('logout/',logout_view,name = 'logout')
 ]
--- todo/task/views.py
+++ todo/task/views.py
@@ -2,30 +2,32 @@ from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Task
 from .forms import TaskForm
-from django.contrib.auth.forms import UserCreationForm,login
-
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 
+@login_required
 def task_list(request):
     status_filter = request.GET.get('status','all')
     category_filter = request.GET.get('category','all')
-    task = Task.objects.filter(user=request.user)
+    tasks = Task.objects.filter(user=request.user)
     if status_filter !='all':
         tasks = tasks.filter(is_compleated=(status_filter == 'compleated'))
     if category_filter !='all':
         tasks = tasks.filter(category=category_filter)
     
-    compleated_tasks = tasks.filter(is_compleated = True)
-    pending_tasks = tasks.filter(is_compleated = False)
+    completed_tasks = tasks.filter(is_completed = True)
+    pending_tasks = tasks.filter(is_completed = False)
 
-    return render(request,'',{
+    return render(request,'task_list.html',{
         'status_filter':status_filter,
         'category_filter':category_filter,
-        'compleated_tasks':compleated_tasks,
+        'compleated_tasks':completed_tasks,
         'pending_tasks':pending_tasks,
     })
 
+@login_required
 def task_create(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
@@ -33,10 +35,10 @@ def task_create(request):
             task = form.save(commit=False)
             task.user = request.user
             task.save()
-            return redirect('')
+            return redirect('task_list')
     else:
         form = TaskForm()
-    return render(request,'',{'form':form})
+    return render(request,'task_create.html',{'form':form})
 
 def task_detail(request,task_id):
     task = get_object_or_404(Task, id = task_id, user=request.user)
@@ -59,9 +61,30 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request,user)
-            return redirect('')
+            return redirect('task_list')
     else:
         form = UserCreationForm()
-    return render(request,'',{'form':form})
+    return render(request,'registration/register.html',{'form':form})
 
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('task_list')
+            else:
+                return render(request,'registration/login.html',{'form':form}) 
+    else:  
+        form = AuthenticationForm() 
+    return render(request,'registration/login.html',{'form':form})
+
+def logout_view(request):
+        logout(request)
+        # Redirect to a desired page after logout, e.g., the login page or homepage
+        return redirect('login') # Assuming 'login' is the name of your login URL pattern
 
