e167e192ba202151d58f8cc183b437541a7b6869
TamimurRahman <tamimr008@gmail.com>
2025-11-16 19:47:10 +0600

    user authentication form or user creation form

--- todo/task/views.py
+++ todo/task/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render,redirec,get_object_or_404
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Task
 from .forms import TaskForm
+from django.contrib.auth.forms import UserCreationForm,login
 
 
 # Create your views here.
@@ -24,4 +25,43 @@ def task_list(request):
         'compleated_tasks':compleated_tasks,
         'pending_tasks':pending_tasks,
     })
+
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect('')
+    else:
+        form = TaskForm()
+    return render(request,'',{'form':form})
+
+def task_detail(request,task_id):
+    task = get_object_or_404(Task, id = task_id, user=request.user)
+    return render(request,'',{'task':task})
+
+def task_delete(request,task_id):
+    task = get_object_or_404(Task,id = task_id,user=request.user)
+    task.delete()
+    return redirect('')
+
+def task_mark_compleated(request,task_id):
+    task = get_object_or_404(Task,id = task_id,user=request.user)
+    task.is_compleated =True
+    task.save()
+    return redirect('')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect('')
+    else:
+        form = UserCreationForm()
+    return render(request,'',{'form':form})
+
 
